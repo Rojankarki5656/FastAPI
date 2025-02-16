0e0d94b57ba6e6f316d0bfecb2bf02e3190f3f10
Rojan Karki <103635523+Rojankarki5656@users.noreply.github.com>
2025-02-16 23:01:16 +0545

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+set -e  # Exit on error
+
 # Install system dependencies
 apt-get update && apt-get install -y cmake libboost-all-dev
+
 # Install Python dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
